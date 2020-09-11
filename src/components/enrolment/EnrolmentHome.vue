358b016b2eb47b73bcf1d149b720f889c3623de8
Harry Maynard <harry.maynard@maximusbc.ca>
2020-09-11 18:21:58 -0400

    Added input field.

--- src/components/enrolment/EnrolmentHome.vue
+++ src/components/enrolment/EnrolmentHome.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>See if you qualify for public health care in B.C.</h1>
     <p>If you already have Medical Services Plan (MSP) coverage and received a letter notifying you to renew your BC Services Card, you do not have to submit a new Application for MSP Enrolment; you only need to renew your BC Services Card. Follow the instructions on the letter to renew your card.</p>
+    <hr/>
+    <p><label>Do you currently live in British Columbia (i.e. Do you have an address here)?</label></p>
+
+    <Radio id="radio1" name="radio" label="No" v-on:radio-select="handleSelect($event)" />
+    <Radio id="radio2" name="radio" label="Yes" v-on:radio-select="handleSelect($event)" />
+
     <Button label="Continue"
             styling="bcgov-normal-blue btn"
             v-on:button-click='nextPage' />
@@ -10,13 +16,20 @@
 
 <script>
 import Button from 'vue-shared-components/src/components/button/Button';
+import Radio from 'vue-shared-components/src/components/radio/Radio';
 
 export default {
-  name: 'HelloWorld',
-  components: {Button},
+  name: 'EnrolmentHome',
+  components: {
+    Button,
+    Radio
+  },
   methods: {
     nextPage: function () {
       this.$router.push('/msp/enrolment/personal-info')
+    },
+    handleSelect: function(event) {
+      console.log('event: ', event);
     }
   }
 }
--- src/components/enrolment/EnrolmentPersonalInfo.vue
+++ src/components/enrolment/EnrolmentPersonalInfo.vue
@@ -1,15 +1,40 @@
 <template>
   <div>
-    Personal Information
+    <h1>Add personal information and upload documents</h1>
+    <h2>Main Applicant</h2>
+    <p>After enrolment, the main applicant will be the Medical Services Plan Account Holder, and will be responsible for maintaining the Medical Services Plan account and requesting any changes.</p>
+    <hr/>
+    <Input
+      v-bind:label="'First Name'"
+      v-bind:id="'textInputId'"
+      v-bind:placeholder="'Enter value'"
+      v-bind:styling="'bcgov-editable-white'"
+      v-bind:isRequired="true"
+      v-bind:value='firstName'
+      v-on:input-change='inputChange'
+    />
+    {{firstName}}
   </div>
 </template>
 
 <script>
+import Input from 'vue-shared-components/src/components/input/Input';
+
 export default {
-  name: 'HelloWorld',
-  components: {},
+  name: 'EnrolmentPersonalInfo',
+  components: {
+    Input
+  },
+  data: () => {
+    return {
+      firstName: null
+    };
+  },
   methods: {
-    continue: function () {}
+    nextPage: function () {},
+    inputChange: function() {
+      console.log("Input Changed: ", this.firstName);
+    }
   }
 }
 </script>
