5c06d4544f1aa642fc54b18873e74b68f22d46c2
Harry Maynard <harry.maynard@maximusbc.ca>
2020-09-18 16:04:08 -0400

    Reset fields when pressing back button.

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import DataService from './services/data-service';
 import Enrolment from './components/enrolment/Enrolment';
 import routes from './routes';
 import pageStateService from './services/page-state-service';
+
 const router = new VueRouter({
   mode: 'history',
   routes: [
@@ -15,7 +16,7 @@ const router = new VueRouter({
     { 
       path: '/msp/enrolment',
       component: Enrolment,
-      redirect: '/msp/enrolment/home',
+      redirect: routes.ENROLMENT_HOME.path,
       children: [
         {...routes.ENROLMENT_HOME},
         {...routes.ENROLMENT_PERSONAL_INFO},
@@ -31,6 +32,7 @@ router.beforeEach((to, from, next) => {
   if ( (to.name !== routes.ENROLMENT_HOME.name && !DataService.hasAcceptedTerms)
     || (to.name !== routes.ENROLMENT_HOME.name && !pageStateService.isPageComplete(to.path))) {
     next({ name: routes.ENROLMENT_HOME.name });
+    DataService.reset();
   } else {
     next();
   }
--- src/services/data-service.js
+++ src/services/data-service.js
@@ -3,6 +3,13 @@ const dataService = {
   lastName: null,
   phn: '',
   hasAcceptedTerms: false,
+
+  reset: function() {
+    this.firstName = null;
+    this.lastName = null;
+    this.phn = null;
+    this.hasAcceptedTerms = false;
+  },
 };
 
 export default dataService;
