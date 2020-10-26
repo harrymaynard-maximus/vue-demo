d40804b77ce412f1bd80f46dd255a0ac0e7f5158
Harry Maynard <harry.maynard@maximusbc.ca>
2020-10-26 11:19:00 -0400

    Add SVGs.

--- src/modules/common/components/file-uploader/FileUploader.vue
+++ src/modules/common/components/file-uploader/FileUploader.vue
@@ -3,10 +3,8 @@
     <div class="dropzone" ref="dropZone">
 
     <div class="instruction-zone d-flex align-items-center flex-wrap flex-sm-nowrap flex-column flex-sm-row">
-      <i class="fa fa-cloud-upload fa-4x d-inline-block upload-icon" ref="imagePlaceholderRef" tabindex="0"></i>
-      <!-- <font-awesome-icon :icon="['fa', 'cloud-upload-alt']" size="3x" /> -->
+      <img :src="cloudUploadIconSvg" class="svg-icon d-inline-block mb-3 ml-3" />
 
-      <img :src="cloudUploadIconSvg" class="svg-icon" />
       <input type="file" :id="id"
               ref="browseFileRef" ngModel accept="image/*,application/pdf" style="display:none;"
               tabindex="0" multiple :name='id'
@@ -24,7 +22,7 @@
         <Thumbnail :imageObject="imageModel" @delete="deleteImage(imageModel)" />
       </div>
 
-        <div class="common-thumbnail" @click='openFileDialog()'>
+        <a href="javascrip: void 0;" class="common-thumbnail ml-3" @click='openFileDialog($event)'>
           <div class="thumbnail-container">
             <div class="image-thumbnail demo-thumbnail">
               <img :src="plusIconSvg" class="svg-icon" />
@@ -33,7 +31,7 @@
               Add
             </div>
           </div>
-        </div>
+        </a>
       </div>
     </div>
 
@@ -45,14 +43,12 @@
 </template>
 
 <script>
-import Vue from 'vue';
 import Thumbnail from './Thumbnail.vue';
-import { Observable ,  Observer, fromEvent, merge } from 'rxjs';
+import { Observable, fromEvent, merge } from 'rxjs';
 import {map, filter, flatMap, scan, delay, retryWhen} from 'rxjs/operators';
-import { CommonImage, CommonImageError, CommonImageProcessingError,
-CommonImageScaleFactors, CommonImageScaleFactorsImpl } from './models/images.js';
-import plusIconSvg from './images/plus.svg'
-import cloudUploadIconSvg from './images/cloud-upload-alt.svg'
+import plusIconSvg from './images/plus.svg';
+import cloudUploadIconSvg from './images/cloud-upload-alt.svg';
+import { v4 as uuidv4 } from "uuid";
 
 var loadImage = require('blueimp-load-image');
 var sha1 = require('sha1');
@@ -60,16 +56,108 @@ const PDFJS = require('pdfjs-dist/build/pdf');
 var pdfJsWorker = require('pdfjs-dist/build/pdf.worker.entry');
 PDFJS.workerSrc = pdfJsWorker;
 
+
+var CommonImageError;
+(function (CommonImageError) {
+    CommonImageError[CommonImageError["WrongType"] = 0] = "WrongType";
+    CommonImageError[CommonImageError["TooSmall"] = 1] = "TooSmall";
+    CommonImageError[CommonImageError["TooBig"] = 2] = "TooBig";
+    CommonImageError[CommonImageError["AlreadyExists"] = 3] = "AlreadyExists";
+    CommonImageError[CommonImageError["Unknown"] = 4] = "Unknown";
+    CommonImageError[CommonImageError["CannotOpen"] = 5] = "CannotOpen";
+    CommonImageError[CommonImageError["PDFnotSupported"] = 6] = "PDFnotSupported";
+    CommonImageError[CommonImageError["CannotOpenPDF"] = 7] = "CannotOpenPDF";
+})(CommonImageError || (CommonImageError = {}));
+
+
+class CommonImageProcessingError {
+  commonImage;
+  rawImageFile;
+  maxSizeAllowed;
+  errorCode;
+  // added errorDescription.PDF.JS gives proper error messages as invalid pdf structure or password protected pdf.Good for splunk tracking
+  constructor(errorCode) {
+    this.errorCode = errorCode;
+  }
+}
+
+/**
+ * Image as uploaded by user
+ */
+class CommonImage {
+  uuid;
+
+  /**
+   * @param fileContent (optional) The base64 of an image. See `fileContent` property.
+   */
+  constructor(fileContent) {
+    this.uuid = uuidv4();
+    if (fileContent) {
+      this.fileContent = fileContent;
+    }
+  }
+
+  /**
+   * The base64 content of an image.  Must already be base64 or some other stringable data-type.
+   * 
+   * You should be able to do <img src='myCommonImage.fileContent'> to render the image.
+   */
+  fileContent = '';
+  documentType = {};
+
+  /**
+   * ContentType should generally match the MIME type, but can be set as needed by application.
+   * https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
+   */
+  contentType = '';
+  // number of bytes.
+  size = 0;
+  sizeUnit = '';
+  sizeTxt = '';
+  naturalHeight = 0;
+  naturalWidth = 0;
+  name = '';
+  // file uniqness checksum
+  id = '';
+  error;
+  errorDescription;
+  attachmentOrder = 0;
+
+  /**
+   * Returns the JSON of an image ready to be submitted to the API.  You may
+   * have to set attachmentOrder before calling this.
+   */
+  toJSON() {
+    return {
+      attachmentOrder: this.attachmentOrder, // will be 0 - should it be 1?
+      attachmentUuid: this.uuid,
+      attachmentDocumentType: this.documentType
+    };
+  }
+}
+
+class CommonImageScaleFactorsImpl {
+  widthFactor;
+  heightFactor;
+
+  constructor(wFactor, hFactor) {
+    this.widthFactor = wFactor;
+    this.heightFactor = hFactor;
+  }
+
+  scaleDown(scale) {
+    return new CommonImageScaleFactorsImpl(
+      this.widthFactor * scale,
+      this.heightFactor * scale);
+  }
+}
+
 export default {
   name: 'FileUploader',
   components: {
     Thumbnail
   },
   props: {
-    noIdImage: {
-      type: Boolean,
-      default: false
-    },
     images: {
       type: Array,
       default: () => []
@@ -78,17 +166,13 @@ export default {
       type: String,
       default: ''
     },
-    showError: {
-      type: Boolean,
-      default: false
-    },
     required: {
       type: Boolean,
       default: false
     },
     instructionText: {
       type: String,
-      default: 'Please upload required ID documents.'
+      default: 'Please upload your documents.'
     }
   },
   data: () => {
@@ -133,7 +217,7 @@ export default {
       /**
        * Must cancel the dragover event in order for the drop event to work.
        */
-      dragOverStream.pipe(map(evt => {
+      dragOverStream.pipe(map(event => {
           return event;
       })).subscribe(evt => {
           if (evt) {
@@ -216,29 +300,11 @@ export default {
               this.errorMessage = '';
           },
 
-          (error) => {
-              
-          },
+          () => {},
           () => {
               console.log('completed loading image');
           }
       );
-
-      const imagePlaceholderEnterKeyStream = merge(
-        fromEvent(this.$refs.imagePlaceholderRef, 'keyup'),
-      ).pipe(filter((evt) => evt.key === 'Enter'));
-
-      merge(
-          fromEvent(this.$refs.imagePlaceholderRef, 'click'),
-          imagePlaceholderEnterKeyStream
-      ).pipe(
-          map((event) => {
-              event.preventDefault();
-              return event;
-          })
-      ).subscribe( (event) => {
-          this.$refs.browseFileRef.dispatchEvent(new MouseEvent('click'));
-      });
   },
 
   methods: {
@@ -262,7 +328,8 @@ export default {
     },
 
     /** Opens the file upload dialog from the browser. */
-    openFileDialog: function() {
+    openFileDialog: function(event) {
+        event.preventDefault();
         console.log('opening file dialog');
         this.$refs.browseFileRef.dispatchEvent(new MouseEvent('click'));
     },
@@ -291,7 +358,6 @@ export default {
 
         // Create our observer
         const fileObservable = Observable.create((observer) => {
-            const imageModels = [];
             scaleFactors = scaleFactors.scaleDown(reductionScaleFactor);
 
             for (let fileIndex = 0; fileIndex < fileList.length; fileIndex++) {
@@ -309,14 +375,14 @@ export default {
                      *
                      *  */
                     this.readPDF(file, pdfScaleFactor, (images , pdfFile) => {
-                        images.map((image, index) => {
+                        images.map((image) => {
                             image.name = pdfFile.name;
                             this.resizeImage(image, self, scaleFactors, observer, pageNumber , true); // index starts from zero
                             pageNumber = pageNumber + 1  ;
                         });
                     }, (error) => {
                         console.log('error' + JSON.stringify(error));
-                        const imageReadError =  new CommonImageProcessingError(CommonImageError.CannotOpenPDF, error);
+                        const imageReadError =  new CommonImageProcessingError(CommonImageError.CannotOpenPDF);
                         self.filterError(imageReadError);
                     });
                 } else {
@@ -365,9 +431,9 @@ export default {
         // Scale the image by loading into a canvas
 
         console.log('Start scaling down the image using blueimp-load-image lib: ');
-        const scaledImage = loadImage(
+        loadImage(
             image.src, // NOTE: we pass the File ref here again even though its already read because we need the XIFF metadata
-            function (canvas, metadata) {
+            function (canvas) {
 
                 // Canvas may be an Event when errors happens
                 if (canvas instanceof Event) {
@@ -384,7 +450,6 @@ export default {
 
                         const fileName = imageModel.name;
                         const nBytes = imageModel.size;
-                        let fileSize = '';
                         let fileSizeUnit = '';
                         let sOutput = nBytes + ' bytes';
                         // optional code for multiples approximation
@@ -392,7 +457,6 @@ export default {
                                 nMultiple = 0, nApprox = nBytes / 1024; nApprox > 1; nApprox /= 1024, nMultiple++) {
 
                             sOutput = nApprox.toFixed(3) + ' ' + aMultiples[nMultiple] + ' (' + nBytes + ' bytes)';
-                            fileSize = nApprox.toFixed(0);
                             fileSizeUnit = aMultiples[nMultiple];
                             imageModel.sizeUnit = fileSizeUnit;
                         }
@@ -450,7 +514,7 @@ export default {
         return function (errors) {
             return errors.pipe(scan(
                 // return errors.pipe(
-                (acc, error, index) => {
+                (acc, error) => {
                     /**
                      * If the error is about file too big and we have not reach max retry
                      * yet, theyt keep going to scaling down.
@@ -521,8 +585,7 @@ export default {
         const imgElsArray = [];
         const ctx = canvas.getContext('2d');
 
-        reader.onload = function (progressEvt) {
-
+        reader.onload = function () {
             const docInitParams = {data: reader.result};
             // TODO - The 'as any' was added when porting to common library from MSP
             const loadingTask = PDFJS.getDocument((docInitParams));
@@ -614,10 +677,9 @@ export default {
             console.log(`Max number of image file you can upload is ${50}.
       This file ${imageModel.name} was not uploaded.`);
         } else {
-            this.images.push(imageModel);
-            this.$emit('input', this.images);
-            this.showError = false;
-            this.noIdImage = false;
+            const images = this.images;
+            images.push(imageModel);
+            this.$emit('input', images);
         }
     },
 
@@ -656,6 +718,7 @@ export default {
         }
         // just add the error to imageModel
         imageModel.error = error;
+        imageModel.errorDescription = errorDescription;
 
         console.log("error with image: ", imageModel);
         this.errorMessage = this.getErrorMessage(imageModel.error);
@@ -695,53 +758,16 @@ export default {
 
     deleteImage: function(imageModel) {
         this.resetInputFields();
-        const index = this.images.findIndex(x => x.uuid === imageModel.uuid);
-        this.images.splice(index, 1);
+        const images = this.images;
+        const index = images.findIndex(x => x.uuid === imageModel.uuid);
+        images.splice(index, 1);
 
         // If there are no images yet, we have to reset the input so it triggers 'required'.
         if ( this.required && this.images.length <= 0 ) {
             console.log('No images, resetting input');
         }
-        this.$emit('input', this.images);
+        this.$emit('input', images);
     },
-
-    /**
-     * Log image attributes
-     * @param imageModel
-     */
-    logImageInfo: function(title, applicationId, imageModel, additionalInfo) {
-
-        // TODO!
-        // // create log entry
-        // const log: LogEntry = new LogEntry();
-        // log.applicationId = applicationId;
-        // const now = moment();
-        // log.mspTimestamp = now.toISOString();
-        // log.applicationPhase = title + ':  imageModelId: ' + imageModel.id
-        //     + '  imageModelUuid: ' + imageModel.uuid
-        //     + '  imageModelSize: ' + imageModel.size
-        //     + '  imageModelWidth: ' + imageModel.naturalWidth
-        //     + '  imageModelHeight: ' + imageModel.naturalHeight
-        //     + '  imageModelContentType: ' + imageModel.contentType
-        //     + (additionalInfo ? '  ' + additionalInfo : '');
-
-        // // send it while subscribing to response
-        // this.logService.logIt(log, title).subscribe(
-        //     (response) => {
-        //         // console.log('log rest service response: ');
-        //         // console.log(response);
-        //     },
-        //     (error) => {
-        //         console.log('HTTP error response from logging service: ');
-        //         console.log(error);
-        //     },
-        //     () => {
-        //         // console.log('log rest service completed!');
-        //     }
-        // );
-    },
-
-
 
     /**
      * Return true if the image size is within range
